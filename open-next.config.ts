b5c21dc41c2248dda2ed67b72f550e0ac22e3288
bahaeeddinechatt-hub <bahaeeddinechatt@icloud.com>
2026-07-12 00:44:25 +0100

    fix open-next config

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,6 +3,21 @@ const config = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
