025ad2b69b750280f9d35b5e077d066f4bc172f2
bahaeeddinechatt-hub <bahaeeddinechatt@icloud.com>
2026-07-17 18:11:04 +0100

    switch to next-on-pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
-
-if (process.env.NODE_ENV === "development") {
-  initOpenNextCloudflareForDev();
-}
 
 const nextConfig: NextConfig = {};
 
